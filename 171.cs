78679fc86887dccfe13675ff20b2d93392d69de2
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2025-12-21 09:50:48 +0530

    171. Excel Sheet Column Number

---
+++ 171.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int TitleToNumber(string columnTitle) {
+       var value = 0;
+ foreach (var ch in columnTitle)
+ {
+     value *= 26;
+     value += ch - 'A' + 1;
+ }
+
+ return value;
+    }
+}
