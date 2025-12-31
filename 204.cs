5da672f56c13da3c2615042e7dce5e4c18a60efc
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2025-12-31 08:56:30 +0530

    204. Count Primes

---
+++ 204.cs
@@ -0,0 +1,25 @@
+public class Solution 
+{
+    public int CountPrimes(int n)
+    {
+        var count = 0;
+        var isComposite = new bool[n + 1];
+
+        for (var i = 2; i < n; i++)
+        {
+            if (isComposite[i])
+            {
+                continue;
+            }
+
+            count++;
+
+            for (var j = i * 2; j < n; j += i)
+            {
+                isComposite[j] = true;
+            }
+        }
+
+        return count;
+    }
+}
