266bdc91ed2bd0f4ad51bde1c75e6ec902c62d1e
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-01-29 20:25:16 +0530

    275. H-Index II

---
+++ 275.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int HIndex(int[] citations) {
+        int n = citations.Length;
+int left = 0, right = n - 1;
+
+while (left <= right)
+{
+    var mid = left + (right - left) / 2;
+    if (citations[mid] == n - mid) return n - mid;
+    else if (citations[mid] < n - mid) left = mid + 1;
+    else right = mid - 1;
+}
+
+return n - left;
+    }
+}
