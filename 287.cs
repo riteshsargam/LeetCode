1f77b69a67d0498514e7c7132594bf9a2b66a128
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-02-05 16:12:30 +0530

    287. Find the Duplicate Number

---
+++ 287.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int FindDuplicate(int[] nums) {
+        int slowPointer = nums[0];
+        int fastPointer = nums[0];
+
+        // Step 1: Detect cycle
+        do
+        {
+            slowPointer = nums[slowPointer]; // Move 1 step
+            fastPointer = nums[nums[fastPointer]]; // Move 2 steps
+        } while (slowPointer != fastPointer);
+
+        // Step 2: Find the entrance to the cycle
+        slowPointer = nums[0];
+        while (slowPointer != fastPointer)
+        {
+            slowPointer = nums[slowPointer];
+            fastPointer = nums[fastPointer];
+        };
+
+        return slowPointer;
+    }
+}
