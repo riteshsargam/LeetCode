93d92f0ba6c1e80ae2e355f321a68acdb11cd72c
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-02-18 08:49:45 +0530

    312. Burst Balloons

---
+++ 312.cs
@@ -0,0 +1,23 @@
+public class Solution {
+  public int MaxCoins(int[] nums) {
+      int n = nums.Length;
+      int[] arr = new int[n + 2];
+      arr[0] = 1;
+      arr[n + 1] = 1;
+      for (int i = 0; i < n; i++) arr[i + 1] = nums[i];
+
+      int[,] dp = new int[n + 2, n + 2];
+
+      for (int len = 2; len <= n + 2; len++) {
+          for (int left = 0; left + len <= n + 1; left++) { 
+              int right = left + len;
+              for (int k = left + 1; k < right; k++) {
+                  dp[left, right] = Math.Max(dp[left, right],
+                      dp[left, k] + dp[k, right] + arr[left] * arr[k] * arr[right]);
+              }
+          }
+      }
+
+      return dp[0, n + 1];
+   }
+}
