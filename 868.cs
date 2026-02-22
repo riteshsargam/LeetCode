c57a83ca059cf8a2815e9238553a536d79af6451
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-02-22 14:57:29 +0530

    868. Binary Gap

---
+++ 868.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int BinaryGap(int n) {
+
+        int ans = 0;
+        int pos = 0;
+        int last = -1;
+
+        while (n > 0) {
+
+            if ((n & 1) == 1) {
+                if (last != -1)
+                    ans = Math.Max(ans, pos - last);
+
+                last = pos;
+            }
+
+            n >>= 1;
+            pos++;
+        }
+
+        return ans;
+    }
+}
