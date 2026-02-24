a6377803925dab905a9e4eac8ec858bbdab32071
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-02-24 19:47:17 +0530

    1022. Sum of Root To Leaf Binary Numbers

---
+++ 1022.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+     public int SumRootToLeaf(TreeNode root)
+   {
+       var sum = 0;
+       Helper(root, 0, ref sum);
+       return sum;
+   }
+
+   private void Helper(TreeNode node, int current, ref int sum)
+   {
+       if (node == null) return;
+
+       current = current * 2 + node.val;
+       if (node.left == null && node.right == null)
+       {
+           sum += current;
+           return;
+       }
+
+       Helper(node.left, current, ref sum);
+       Helper(node.right, current, ref sum);
+   }
+}
