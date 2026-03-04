bd935fcf6322b5d968c013470d3d0f2b877f95ce
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-03-04 12:07:27 +0530

    1582. Special Positions in a Binary Matrix

---
+++ 1582.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int NumSpecial(int[][] mat) {
+        int m = mat.Length;
+        int n = mat[0].Length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
