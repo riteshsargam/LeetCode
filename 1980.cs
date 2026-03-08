3a6d750de5858c9f0a74654a48cd72b3b9b4bd19
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-03-08 09:56:57 +0530

    1980. Find Unique Binary String

---
+++ 1980.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string FindDifferentBinaryString(string[] nums) {
+        HashSet<string> set = new HashSet<string>(nums);
+        int n = nums.Length;
+        char[] result = new char[n];
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = nums[i][i] == '0' ? '1' : '0';
+        }
+        
+        return new string(result);
+    }
+}
