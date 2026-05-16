db319bdfba89b4e5cf28fa835dce3a3b0847e44d
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-05-16 10:35:13 +0530

    154. Find Minimum in Rotated Sorted Array II

---
+++ 154.1.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int FindMin(int[] nums) {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == nums[right]) {
+                right--;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return nums[left];
+    }
+}
