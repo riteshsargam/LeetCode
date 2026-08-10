59232c767c0be18bb8afed7b0adb2bb443ebd9c9
Ritesh Sargam <113118850+riteshsargam@users.noreply.github.com>
2026-08-10 08:27:48 +0530

    1510. Stone Game IV

---
+++ 1510.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool WinnerSquareGame(int n) {
+        bool[] dp = new bool[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+}
